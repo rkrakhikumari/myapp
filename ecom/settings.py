194eedce3a129f5a20b5d07485ec535dad91f1c9
rkrakhikumari <rkrakhikumari012@gmail.com>
2025-04-19 18:58:48 +0530

    final

--- ecom/settings.py
+++ ecom/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_s0s7g_8sgzhwet&#cm9e9gvk25tvlhof52si4#u*877aw80xe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [ '127.0.0.1', '.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,23 +73,23 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default' : {
-        'ENGINE' : 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres' ,
-        'PASSWORD': 'EOVQPjeEhfjzffFfOReYNVpnIHZjsZHx',
-        'HOST': 'mainline.proxy.rlwy.net' ,
-        'PORT': '19546',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default' : {
+#         'ENGINE' : 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres' ,
+#         'PASSWORD': 'EOVQPjeEhfjzffFfOReYNVpnIHZjsZHx',
+#         'HOST': 'mainline.proxy.rlwy.net' ,
+#         'PORT': '19546',
+#     }
+# }
 
 
 # Password validation
